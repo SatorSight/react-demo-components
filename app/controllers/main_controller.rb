66f766244234ff451958128ba7dc870946202331
satorsight <satorsight@gmail.com>
2018-01-29 12:57:38 +0300

    done

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,13 @@
 class MainController < ApplicationController
-
   def index
-    @props = {}
-
+    # detect all components defined in react-demo js bundle
+    @components = []
+    components_file_contents = File.open('app/javascript/packs/react-demo.js').read
+    components_ar = components_file_contents.scan /import.(.*?).from/im
+    components_ar.each{|c_ar| @components.push c_ar.first unless c_ar.first == 'ReactOnRails'}
   end
 
+  def component
+    @props = { name: params[:name].to_s }
+  end
 end
--- app/javascript/packs/react-demo.js
+++ app/javascript/packs/react-demo.js
@@ -1,6 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 import IssuesSwiper from '../bundles/react_demo/components/IssuesSwiper';
+import PopularArticles from '../bundles/react_demo/components/PopularArticles';
 
 ReactOnRails.register({
-    IssuesSwiper
+    IssuesSwiper,
+    PopularArticles
 });
