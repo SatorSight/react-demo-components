c67d2a2c66a4b74589ac0f587c84fb3aa99c13e1
Lud0k <L!9920k>
2018-01-30 17:57:26 +0300

    add PopularJournalsSwiper

--- app/javascript/packs/react-demo.js
+++ app/javascript/packs/react-demo.js
@@ -4,11 +4,13 @@ import PopularArticles from '../bundles/react_demo/components/PopularArticles';
 import OtherIssues from '../bundles/react_demo/components/OtherIssues';
 import NextArticle from '../bundles/react_demo/components/NextArticle';
 import Article from '../bundles/react_demo/components/Article';
+import PopularJournalsSwiper from '../bundles/react_demo/components/PopularJournalsSwiper';
 
 ReactOnRails.register({
     IssuesSwiper,
     NextArticle,
     PopularArticles,
     OtherIssues,
-    Article
+    Article,
+    PopularJournalsSwiper
 });
